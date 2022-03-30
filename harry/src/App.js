e87e29753c0bca95cd9a6b7410a6cbb7d0536347
JessicaW23 <woudsje@gmail.com>
2022-03-30 10:50:03 +0200

    Opmaak van "homepagina"
    In App.js 'posts' veranderd naar 'characters'. P tags gemaakt en classNames gegeven aan name, gender en house. In App.css een grid toegevoegd zodat naam, gender en house in vakken komen te staan.

--- harry/src/App.js
+++ harry/src/App.js
@@ -4,26 +4,29 @@ import "./App.css";
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { posts: [] };
+    this.state = { characters: [] };
   }
 
   componentDidMount() {
     //api call
     fetch("http://hp-api.herokuapp.com/api/characters")
       .then((resp) => resp.json())
-      .then((resp) => this.setState({ posts: resp }));
+      .then((resp) => this.setState({ characters: resp }));
   }
   render() {
     return (
-      <ul>
-       
-        {this.state.posts.map((post) => (
+      <li className="App-grid">
+        {this.state.characters.map((post) => (
           <li key={post.id}>
             {post.id}
-            <p>Name:{post.name} . Gender:{post.gender} . House:{post.house}</p>
+            <a>
+              <p className="name">Name:{post.name}</p>{" "}
+              <p className="gender">Gender:{post.gender}</p>
+              <p className="house">House:{post.house}</p>
+            </a>
           </li>
         ))}
-      </ul>
+      </li>
     );
   }
 }
