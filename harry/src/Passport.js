864ed7cd9128de07cf5e22ff152c2f25169acc2e
ruthkorsaan <r.korsaan@st.hanze.nl>
2022-04-05 14:09:08 +0200

    opmaak paspoort uiklap

--- harry/src/Passport.js
+++ harry/src/Passport.js
@@ -17,29 +17,25 @@ function Passport(props) {
       className={`${showDetails ? "Passport active" : "Passport"}`}
       onClick={onClick}
     >
-      <button classname="button2paspoort">show/hide</button>
+      
 
       {showDetails ? (
         <div className="Passport-details">
-          <h4>Details</h4>
           {props.name}
-          <p className="name">
-            Name:{props.character.name} - {props.character.alternate_names}
-          </p>
-          <p className="date">Birth date:{props.character.dateOfBirth}</p>
-          <p className="species">Species:{props.character.species}</p>
-          <p className="gender">Gender:{props.character.gender}</p>
-          <p className="ancestry">Ancestry:{props.character.ancestry}</p>
-          <p className="eye">Eye colour:{props.character.eyeColour}</p>
+          <img className="foto" src={props.character.image}></img>
+          <p className="name">Name: {props.character.name}</p>
+          <p className="date">Birth date: {props.character.dateOfBirth}</p>
+          <p className="species">Species: {props.character.species}</p>
+          <p className="gender">Gender: {props.character.gender}</p>
+          <p className="ancestry">Ancestry: {props.character.ancestry}</p>
+          <p className="eye">Eye colour: {props.character.eyeColour}</p>
           <p className="hair">Haircolour: {props.character.hairColour}</p>
           <p className="wand">
-            Wand:{props.character.wand.wood} - {props.character.wand.core} -{" "}
+            Wand: {props.character.wand.wood} - {props.character.wand.core} -{" "}
             {props.character.wand.length}
           </p>
-          <p className="patronus">Patronus:{props.character.patronus}</p>
-          <p className="foto">
-            <img src={props.character.image}></img>
-          </p>
+          <p className="patronus">Patronus: {props.character.patronus}</p>
+          
         </div>
       ) : null}
     </li>
